9ebc7b2c6c2203069461241826a236122db94149
Ben <ben05allen@hotmail.com>
2022-10-14 18:48:11 +0900

    L-chan's colours

--- js/scipt.js
+++ js/scipt.js
@@ -1,31 +1,32 @@
-
-// let hhmmss = resetStopWatch;
-
-function showStopWatch (hhmmss) {
+window.onload = function () {
     
-    // alert(hhmmss)
-    
-    document.getElementById("StopWatchDisplay").textContent = hhmmss;
+    let timer = '000.00';
+    let interval;
 
-    setTimeout(showStopWatch, 10000);
+    buttonStart.onclick = function () {
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10);
+    }
+ 
+    buttonStop.onclick = function () {
+        clearInterval(interval);
+    }
+
+    buttonReset.onclick = function () {
+        document.getElementById('StopWatchDisplay').innerText = '000.00';
+    }
+
+    function startTimer () {
+        timer = document.getElementById('StopWatchDisplay').innerText;
+        let hundreths = Number(timer.slice(-2));
+        hundreths ++;
+        let separator = timer.indexOf('.');
+        hundreths += 100 * Number(timer.slice(0,separator));
+        timer = String(hundreths).padStart(5, '0');
+
+        document.getElementById('StopWatchDisplay').innerText = timer.slice(0,3) + '.' + timer.slice(-2,);
+    }
+    
+    
 
 }
-
-showStopWatch('00:00:00');
-
-// function startStopWatch(hhmmss) {
-
-
-// }
-
-// function stopStopWatch() {
-
-
-// }
-
-
-// function resetStopWatch() {
-
-//     return '00:00:00'
-
-// }
